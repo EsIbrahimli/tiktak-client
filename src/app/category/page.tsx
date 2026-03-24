27adb63899251d37b3b578f37fc911b0cd76bc6b
mmurad063 <muradmmmdov063@gmail.com>
2026-03-24 04:16:15 +0400

    added new feature

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,10 +1,43 @@
+"use client";
 
-import React from 'react'
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useCategoriesStore } from "@/common/store/categoryStore";
+import styles from "./categories.module.css";
 
-const page = () => {
+export default function CategoriesPage() {
+  const { categories, fetchCategories, loading } = useCategoriesStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchCategories();
+  }, [fetchCategories]);
+
+  if (loading) return <p>Yüklənir...</p>;
+
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.grid}>
+        {categories.map((cat) => (
+          <div
+            key={cat.id}
+            className={styles.card}
+            onClick={() => router.push(`/category/${cat.slug}`)}
+          >
+            <div className={styles.imageWrapper}>
+              <Image
+                src={cat.img_url}
+                alt={cat.name || "category"}
+                width={90}
+                height={90}
+              />
+            </div>
 
-export default page
+            <p className={styles.name}>{cat.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
