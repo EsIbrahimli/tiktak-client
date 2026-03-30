4cbd4a1bac1cbe1dac0088fb3a2380afdd8d14cc
Asmar Ibrahimli <ibrahimli.asmar@gmail.com>
2026-03-30 17:39:31 +0400

    fixed some bugs of myBasket

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { useRouter, useParams } from 'next/navigation';
 import Image from 'next/image';
 import { useCategoriesStore } from '@/common/store/categoryStore';
 import { useProductsStore } from '@/common/store/productsStore';
-import myBasket from '@/common/components/myBasket/page';
+import Basket from '@/common/components/myBasket/page';
 import styles from '../categories.module.css';
 import Loading from '@/common/components/Loading/Loading';
 
@@ -106,7 +106,7 @@ export default function CategoryDetailPage() {
           ))
         )}
       </div>
-      <Basket />
+      <myBasket />
     </div>
 
   );
--- src/common/components/myBasket/page.tsx
+++ src/common/components/myBasket/page.tsx
@@ -2,7 +2,7 @@ import styles from './myBasket.module.css'
 import { RiDeleteBin6Line } from "react-icons/ri";
 import Image from 'next/image';
 
-const Basket = () => {
+const myBasket = () => {
     return (
         <div className={styles.container}>
             <h2 className={styles.basketTitle}>Səbətim</h2>
@@ -39,4 +39,4 @@ const Basket = () => {
     );
 }
 
-export default Basket;
+export default myBasket;
